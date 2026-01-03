d8a0435807442be43ad6eb46e64fe8c1d1762bf7
phonty29 <amedov.bekmuhamet@gmail.com>
2026-01-03 13:20:03 +0500

    refactor: match module

---
+++ src/match.h
@@ -0,0 +1,11 @@
+#ifndef MATCH
+#define MATCH
+
+#include <stdbool.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+bool match_pattern(const char* input_line, const char* pattern);
+
+#endif
